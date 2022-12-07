15ccde6e5ccf9e2bae21116b9df016e60f87be94
Artem Teleshev <artem.teleshev@gmail.com>
2022-12-07 22:28:43 +0200

    updated http-echo-server container

--- http-echo-server/main.go
+++ http-echo-server/main.go
@@ -1,32 +1,64 @@
 package main
 
 import (
-    "sync"
-    "fmt"
-    "net/http"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
+	http.HandleFunc("/", hello)
 
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    wg.Add(1)
-    go runHTTP(":8080", &wg)
+	wg.Add(1)
+	go runHTTPServer(":8080", &wg)
 
-    wg.Add(1)
-    go runHTTP(":9090", &wg)
+	wg.Add(1)
+	go runHTTPServer(":9090", &wg)
 
-    fmt.Println("Started 2 listeners: [8080|9090]")
-    wg.Wait()
+	fmt.Println("Started 2 listeners: [8080|9090]")
+
+	wg.Add(1)
+	go runHTTPChecker(&wg)
+
+	wg.Wait()
 }
 
-func runHTTP(addr string, wg *sync.WaitGroup) {
-    defer wg.Done()
+func runHTTPServer(addr string, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-    http.ListenAndServe(addr, nil)
+	http.ListenAndServe(addr, nil)
+}
+
+func runHTTPChecker(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		time.Sleep(3 * time.Second)
+		runHTTPClient("http://localhost:8080/test-8080")
+		runHTTPClient("http://localhost:9090/test-9090")
+	}
+}
+
+func runHTTPClient(rawURL string) {
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "HTTP GET ERROR %s: %v", rawURL, err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Fprintf(os.Stdout, "HTTP GET OK %s: ", rawURL)
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		fmt.Fprintf(os.Stderr, "HTTP GET ERROR %s: %v", rawURL, err)
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello", r.URL.Path[1:])
+	fmt.Fprintln(w, "Hello", r.URL.Path[1:])
 }
